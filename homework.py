9962134c1449aee4204645c25adcf6476ae27fb3
Alexey Kurepin <kurepin@tut.by>
2022-02-14 23:19:11 +0300

    Fixed

--- homework.py
+++ homework.py
@@ -115,7 +115,8 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        speed = self.length_pool*self.count_pool / self.M_IN_KM / self.duration
+        speed = \
+            self.length_pool * self.count_pool / self.M_IN_KM / self.duration
         return speed
 
     def get_spent_calories(self) -> float:
